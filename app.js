d0f769dd30df48010a187381967a366fdc9381e4
eduardoGrDev <eduardo.granillo.dev@gmail.com>
2024-05-18 18:00:11 -0600

    Actualizacion de style.css y index.html

---
+++ app.js
@@ -0,0 +1,6 @@
+const focusButton = document.getElementById('focusButton');
+const shortBreakButton = document.getElementById('shortBreakButton');
+const longBreakButton = document.getElementById('longBreakButton');
+const timer = document.getElementById('timer');
+const startButton = document.getElementById('startButton');
+
